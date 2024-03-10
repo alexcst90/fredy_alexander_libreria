1e874c2c7d38d069013e28f16580108d2849716b
fcastillo <fcastillo@hightech-corp.com>
2024-03-09 19:01:42 -0600

    subiendo cambios

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -9,7 +9,6 @@ def call(boolean abortOnFailure, boolean abortPipeline) {
             error 'pipeline aborted proccess...'
         }
 
-        echo 'abortOnFailure '+abortOnFailure + 'passed '+ passed + 'abortPipeline' + abortPipeline
         return passed
 }
 
@@ -20,6 +19,6 @@ def executeSonar(){
 
 def qualityGate(result){
     echo 'quality gate, waiting for results'
-   // sleep time: 2, unit: 'MINUTES'
+    sleep time: 2, unit: 'MINUTES'
     return result == 'SUCCESS'
 }
