b17b4009ae370132ba3090fe02021301fc689014
fcastillo <fcastillo@hightech-corp.com>
2024-03-09 18:22:42 -0600

    subiendo cambios

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,7 +1,21 @@
-def call() {
-        sleep time: 3, unit: 'MINUTES'
-        echo "Ejecucion de las pruebas de calidad de codigo."
-    
-    
+def call(boolean abortOnFailure = fales) {
+        //sleep time: 3, unit: 'MINUTES'
+        
+        def result = executeSonar()
+        def timeout = 2 * 60
 
+        def passed = qualityGate(result, timeout)
+
+        return passed
+}
+
+def executeSonar(){
+    echo "Ejecucion de las pruebas de calidad de codigo."
+    return 'SECCESS'
+}
+
+def qualityGate(result, timeout){
+    echo 'quality gate, waiting for results'
+    sleep(timeout)
+    return result = 'SUCCESS'
 }
